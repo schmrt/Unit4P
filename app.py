e828f9a2b8b8b986541389cf0a3221ada02f105e
a02287927@aggies.usu.edu <a02287927@aggies.usu.edu>
2025-02-19 21:12:03 +0000

    Website backup

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+	return render_template("index.html")
+
+@app.route("/aboutme")
+def aboutme():
+	return render_template("aboutme.html")
+
+@app.route("/baja")
+def baja():
+	return render_template("baja.html")
